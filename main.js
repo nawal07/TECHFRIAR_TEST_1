3769d2481a364764913828064f8409bc0e47c32b
nawal07 <nawalsidiq077@gmail.com>
2024-08-18 12:10:37 +0530

    FUNCTIONALITY_UPDATES

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
+document.getElementById('bookCar').addEventListener('click', function (event) {
+  const bookCarMenu = document.getElementById('bookCarMenu');
+  if (bookCarMenu.style.display == 'none') {
+    bookCarMenu.style.display = 'flex';
+  } else {
+    bookCarMenu.style.display = 'none';
+  }
+
+  event.stopPropagation();
+});
+
+document.addEventListener('click', function (event) {
+  const bookCarMenu = document.getElementById('bookCarMenu');
+  const bookCarButton = document.getElementById('bookCar');
+
+  if (
+    !bookCarMenu.contains(event.target) &&
+    !bookCarButton.contains(event.target)
+  ) {
+    bookCarMenu.style.display = 'none';
+  }
+});
+
+document.getElementById('same-pick').addEventListener('click', function () {
+  const differntDropOff = document.getElementById('differnt-drop-location');
+  const samePickUp = document.getElementById('same-pick');
+  const diffDrop = document.getElementById('differnt-drop');
+
+  samePickUp.classList.add('active');
+  samePickUp.classList.remove('inactive');
+  diffDrop.classList.add('inactive');
+  differntDropOff.style.display = 'none';
+});
+
+document.getElementById('differnt-drop').addEventListener('click', function () {
+  const differntDropOff = document.getElementById('differnt-drop-location');
+  const samePickUp = document.getElementById('same-pick');
+  const diffDrop = document.getElementById('differnt-drop');
+
+  samePickUp.classList.add('inactive');
+  diffDrop.classList.remove('inactive');
+  differntDropOff.style.display = 'flex';
+});
+
 Array.prototype.forEach.call(
   document.getElementsByClassName('showDescription'),
   function (element, index) {
@@ -13,13 +57,13 @@ Array.prototype.forEach.call(
         desc.style.display = 'none';
         hideDescription[i].style.display = 'none';
         showDescription[i].style.display = 'block';
-        title[i].classList.remove('show')
+        title[i].classList.remove('show');
       });
 
       description[index].style.display = 'block';
       hideDescription[index].style.display = 'block';
       showDescription[index].style.display = 'none';
-      title[index].classList.add('show')
+      title[index].classList.add('show');
     });
   }
 );
@@ -38,7 +82,7 @@ Array.prototype.forEach.call(
       description[index].style.display = 'none';
       hideDescription[index].style.display = 'none';
       showDescription[index].style.display = 'block';
-      title[index].classList.remove('show')
+      title[index].classList.remove('show');
     });
   }
 );
